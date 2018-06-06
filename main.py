c5dcd2c3c0b114bb303beb8cb1d307f795c09fba
PiotrusWatson <piotruswatson@gmail.com>
2018-06-06 17:44:02 +0100

    added basic main file which prompts user

---
+++ main.py
@@ -0,0 +1,19 @@
+def main():
+	print("Welcome to evogame! What do you want to do?")
+	while (True):
+	    command = input("Enter a command: ")
+	    
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+	main()
