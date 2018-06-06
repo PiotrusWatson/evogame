1f73047845f2e3dbad3343e0b27a513183d9bd4b
PiotrusWatson <piotruswatson@gmail.com>
2018-06-06 18:30:02 +0100

    Placeholder command line interface code created

--- main.py
+++ main.py
@@ -1,9 +1,63 @@
+#Placeholder
+def show_map(void):
+	print("showin map")
+#placeholder
+def move(direction):
+	if (direction == None):
+		print("please enter a direction")
+	else:
+		print("movin in", direction)
+
+def list_species(void):
+	print("listing all species on current biome")
+
+def end_turn(void):
+	print ("ending turn")
+
+def mutate(mutation):
+	if (mutation == None):
+		print("listing available mutations")
+	else:
+		print("mutating to", mutation)
+
+def list_mutations(available):
+	if (available == None):
+		print("showing current mutations")
+	else:
+		print("showing all mutations")
+
+def my_species(void):
+	print("printing stats")
+
+def error(void):
+	print("i do not understand. Commands are: ")
+	for command in command_to_function:
+		print(command)
+
+
+
+command_to_function = {"map": show_map, 
+"move": move,
+"species": list_species, 
+"end_turn": end_turn, 
+"mutate": mutate,
+"mutations": list_mutations,
+"my_species": my_species,
+}
+
 def main():
 	print("Welcome to evogame! What do you want to do?")
 	while (True):
-	    command = input("Enter a command: ")
-	    
+	    entered_text = input("Enter a command: ")
+	    if (" " in entered_text):
+	    	command, argument = entered_text.split(" ")
+	    	
+	    else:
+	    	command = entered_text
+	    	argument = None
 
+	    command_to_function.get(command, error)(argument)
+	    
 
 
 
